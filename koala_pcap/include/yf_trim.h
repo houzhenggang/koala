1bfa9bd36a8fec63dbdc721cb58e2708811847a9
ya_feng_li@163.com <ya_feng_li@163.com>
2014-09-16 12:21:46 +0800

    modify.

--- koala_pcap/include/yf_trim.h
+++
@@ -1,9 +0,0 @@
-#ifndef _YF_TRIM_H
-#define _YF_TRIM_H
-
-#define IS_SPACE(x) ((x)==' '||(x)=='\r'||(x)=='\n'||(x)=='f'||(x)=='\b'||(x)=='\t')
-#define OPTION_BUF_LEN 1024
-
-void trim(const char *str, char *buffer);
-
-#endif
